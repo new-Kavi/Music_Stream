a69f8962b4ce22a673c5b3f17ca2903120d5a65e
Kavindra Chamola <kavindrachamolla889@gmail.com>
2025-07-18 16:39:46 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   if ( document.domain === "localhost" ) {
     __URL__ = "http://localhost:1337";
   } else {
-    __URL__ = "https://music-player-app-backend-yq0c.onrender.com";
+    __URL__ = "https://music-stream-git-main-kavis-projects-34ac54f8.vercel.app/";
   }
 
   const handleSubmit = async (e) =>{
